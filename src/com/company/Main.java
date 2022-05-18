5e39c7ad380805f9843f0b866e82c59d2d08ddd3
mananapatsatsia <mananapatsatsiaqa@gmail.com>
2022-05-17 20:08:50 -0400

    1st commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        SolarSystem FactorX =new SolarSystem();
+        FactorX.Sun="1";
+        FactorX.Moon="4";
+        FactorX.Planet="2";
+        FactorX.Stars="10000";
+
+//        adding features to our sun
+        FeatureSun Xfact= new FeatureSun();
+        Xfact.color="green";
+        Xfact.radius="1000000000km";
+        Xfact.heat="309324934898kj";
+    }
+    }
+
